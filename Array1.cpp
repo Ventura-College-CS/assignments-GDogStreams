78d7b8cd75d1950331dbb1bc33d487b8bf805fa8
Gabriel Meza <gabriel_meza1@my.vcccd.edu>
2021-09-13 14:33:30 -0700

    Fixed it

--- Array1.cpp
+++ Array1.cpp
@@ -3,13 +3,12 @@ using namespace std;
 
 int main()
 {
-    const int [x] = 10;
+    const int x = 10;
     int numbers [x] = {5,7,2,4,6,7,10,1,3,9};
 
-    for(int i=1; i i<x-1; i++)
+    for(int i=1; i<x-1; i++)
     {
         if((numbers[i-1]<numbers[i]) && (numbers[i]<numbers[i+1]))
-        cout << numbers[i] << "It's now in an increasing order";
-        cout << "Test";
+        cout << numbers[i] << " has the increasing order\n";
     }
 }
