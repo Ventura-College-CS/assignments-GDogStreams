f571c831a54428d3c3c9432efed6fa9013dd0bb9
Gabriel Meza <gabriel_meza1@my.vcccd.edu>
2021-09-13 13:45:08 -0700

    Commit 1

---
+++ Array1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    const int [x] = 10;
+    int numbers [x] = {5,7,2,4,6,7,10,1,3,9};
+
+    for(int i=1; i i<x-1; i++)
+    {
+        if((numbers[i-1]<numbers[i]) && (numbers[i]<numbers[i+1]))
+        cout << numbers[i] << "It's now in an increasing order";
+    }
+}
